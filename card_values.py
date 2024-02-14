276a482170f6509db390f7e3df26f1ae65a9f4de
George Loyer <gloyer@sonic.net>
2024-02-14 13:38:59 -0800

    Updated documentation lines

--- card_values.py
+++ card_values.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
+'''
+card-values.py
+History: 2024-02-12 original
+Author: George Loyer
 
-# card-values.py
-# History: 2024-02-12 original
-# Author: George Loyer
-#
-# accepts three user inputs:
-#   a list of space-delimited FITS header card names
-#   a list of space-delimited filenames that are wildcard expanded
-#   the output file name
-#
-# After user input is read in main(), lists are created for the
-# card names and file names, and the output file is opened.
-# The card names are used as the text of the header line in the
-# output file, comma-delimited. Then the read_card_values()
-# function is executed once for each file and the card values
-# are output to the output file, comma delimited. When the
-# program is complete, the output file is a CSV file that can
-# be read by Excel for additional review and analysis.
+Accepts three user inputs:
+  a list of space-delimited FITS header card names
+  a list of space-delimited filenames that are wildcard expanded
+  the output file name
+
+After user input is read in main(), lists are created for the
+card names and file names, and the output file is opened.
+The card names are used as the text of the header line in the
+output file, comma-delimited. Then the read_card_values()
+function is executed once for each file and the card values
+are output to the output file, comma delimited. When the
+program is complete, the output file is a CSV file that can
+be read by Excel for additional review and analysis.
+'''
 
 import sys
 import glob
